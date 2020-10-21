c9a65a7da1388199f34cfd5f2adeb911955eae0b
EC2 Default User <ec2-user@ip-172-31-94-166.ec2.internal>
2020-10-21 03:01:59 +0000

    [Finish]

--- bookers2/app/controllers/relationships_controller.rb
+++ bookers2/app/controllers/relationships_controller.rb
@@ -5,13 +5,13 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:relationship][:following_id])
     # binding.pry
     current_user.follow(@user)
-    redirect_to @user
+    redirect_to users_path
   end
 
   def destroy
     @user = Relationship.find(params[:id]).following
     current_user.unfollow(@user)
-    redirect_to @user
+    redirect_to users_path
   end
 
 end
